fecd8bfe2527ac24f7f2a12a5cc84b92a8660607
lk <lukasz.korycinski@looksoft.pl>
2023-07-22 23:54:23 +0200

    better icon mapping

--- app/src/main/java/com/empik/weather/ui/screens/city_weather/CityWeatherScreen.kt
+++ app/src/main/java/com/empik/weather/ui/screens/city_weather/CityWeatherScreen.kt
@@ -375,8 +375,8 @@ private fun DayPage(
 
 fun getWeatherIcon(iconApi: Int): Int = when(iconApi){
     in 1 .. 2, 30 -> R.drawable.animated_sunny_day
-    in 3 .. 6, 20, 21, 31, 32 -> R.drawable.animated_cloudy_sunny_day
-    in 7 .. 11, 19 -> R.drawable.animated_cloudy_day
+    in 3 .. 5, 20, 21, 31, 32 -> R.drawable.animated_cloudy_sunny_day
+    in 6 .. 11, 19 -> R.drawable.animated_cloudy_day
     in 12 .. 18 -> R.drawable.animated_rainy_day
     in 22 .. 29 -> R.drawable.animated_rainy_day
     else -> R.drawable.animated_sunny_day
