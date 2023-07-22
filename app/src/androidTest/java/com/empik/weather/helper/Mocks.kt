d079e80ef6e28f10cd51c74e527edbb8fd544123
lk <lukasz.korycinski@looksoft.pl>
2023-07-23 01:18:57 +0200

    di in test

--- app/src/androidTest/java/com/empik/weather/helper/Mocks.kt
+++ app/src/androidTest/java/com/empik/weather/helper/Mocks.kt
@@ -2,11 +2,14 @@ package com.empik.weather.helper
 
 import com.empik.weather.data.api.SafeResponse
 import com.empik.weather.data.api.models.response.ForecastResponse
+import com.empik.weather.data.repository.WeatherRepositoryInterface
 import com.empik.weather.data.repository.WeatherRepositoryMocked
 import kotlinx.coroutines.runBlocking
+import org.koin.java.KoinJavaComponent
 
 internal fun getMockedForecast(): ForecastResponse {
-    val weatherRepository = WeatherRepositoryMocked()
+    val weatherRepository =  KoinJavaComponent.get<WeatherRepositoryInterface>(WeatherRepositoryInterface::class.java)
+            as WeatherRepositoryMocked
 
     var mockedResponse: ForecastResponse? = null
     runBlocking {
